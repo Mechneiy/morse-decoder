70ed137a375077e8e1d28756fbf9381b65bdd0db
Vital Hoza <43417456+Mechneiy@users.noreply.github.com>
2023-01-21 19:44:44 +0300

    feat: change file

--- src/index.js
+++ src/index.js
@@ -38,7 +38,20 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+      const Probel = { "**********": " " };
+  const Key_morse = Object.keys(MORSE_TABLE);
+  for (let i = 0; i < Key_morse.length; i++) {
+    Probel[
+      (
+        "0000000000" + Key_morse[i].replace(/\./g, "10").replace(/\-/g, "11")
+      ).slice(-10)
+    ] = MORSE_TABLE[Key_morse[i]];
+  }
+  let text_test = "";
+  for (let i = 0; i < expr.length; i = i + 10) {
+    text_test = text_test + Probel[expr.substring(i, i + 10)];
+  }
+  return text_test;
 }
 
 module.exports = {
